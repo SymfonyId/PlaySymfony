577190cd392138b350be34925151590b163e31ed
Muhammad Surya Ihsanuddin <surya.kejawen@gmail.com>
2016-03-01 17:52:11 +0700

    add HateOas Example

---
+++ src/AppBundle/Controller/HateOasController.php
@@ -0,0 +1,49 @@
+<?php
+namespace AppBundle\Controller;
+
+use Hateoas\Configuration\Route;
+use Hateoas\Representation\CollectionRepresentation;
+use Hateoas\Representation\Factory\PagerfantaFactory;
+use Pagerfanta\Adapter\DoctrineORMAdapter;
+use Pagerfanta\Pagerfanta;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * @Rest\Route("/api")
+ */
+class HateOasController extends Controller
+{
+    /**
+     * @Rest\Get(name="groups_list", path="/groups", defaults={"_format" = "json"})
+     * @Rest\View()
+     */
+    public function getGroupsAction(Request $request)
+    {
+        $limit = $request->query->getInt('limit', 10);
+        $page = $request->query->getInt('page', 1);
+
+        $queryBuilder = $this->getDoctrine()->getManager()->createQueryBuilder()
+            ->select('g')
+            ->from('AppBundle:Group', 'g')
+        ;
+
+        $pagerAdapter = new DoctrineORMAdapter($queryBuilder);
+        $pager = new Pagerfanta($pagerAdapter);
+        $pager->setCurrentPage($page);
+        $pager->setMaxPerPage($limit);
+
+        $pagerFactory = new PagerfantaFactory();
+
+        return $pagerFactory->createRepresentation(
+            $pager,
+            new Route('groups_list', array('limit' => $limit, 'page' => $page)),
+            new CollectionRepresentation(
+                $pager->getCurrentPageResults(),
+                'groups',
+                'groups'
+            )
+        );
+    }
+}
--- src/AppBundle/Entity/Group.php
+++ src/AppBundle/Entity/Group.php
@@ -3,10 +3,15 @@
 namespace AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use JMS\Serializer\Annotation as Serializer;
+use Hateoas\Configuration\Annotation as Hateoas;
 
 /**
  * @ORM\Table(name="pl_group")
  * @ORM\Entity
+ *
+ * @Serializer\XmlRoot("group")
+ * @Hateoas\Relation("self", href = "expr('/api/groups/' ~ object.getId())")
  */
 class Group
 {
@@ -14,6 +19,8 @@ class Group
      * @ORM\Column(name="id", type="integer", nullable=false)
      * @ORM\Id
      * @ORM\GeneratedValue(strategy="AUTO")
+     *
+     * @Serializer\XmlAttribute
      */
     private $id;
 
